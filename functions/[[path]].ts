8cc1101e35e7706caabfb81d617874caeef1110b
Aldi Nugraha <aldi.nugrahatk@gmail.com>
2025-10-01 13:43:01 +0700

    feat(cloudflare): hello

--- functions/[[path]].ts
+++ functions/[[path]].ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 // eslint-disable-next-line import/no-unresolved
-import * as build from "../build";
+import * as build from "../build/server/index.js";
 import { createPagesFunctionHandler } from "@remix-run/cloudflare-pages";
 
 const handleRequest = createPagesFunctionHandler({ build });
